6a8ef07eccb98d2842e6bd4312b2d7ad2f3c3039
Ted Neward <ted@tedneward.com>
2025-06-19 21:46:46 -0700

    Trying to make decorators errors go away

--- typescript/decorators.ts
+++ typescript/decorators.ts
@@ -1,5 +1,48 @@
 namespace Decorators {
 
+function loggedMethod(originalMethod: any, context: ClassMethodDecoratorContext) {
+    const methodName = String(context.name);
+
+    function replacementMethod(this: any, ...args: any[]) {
+        console.log(`LOG: Entering method '${methodName}'.`)
+        const result = originalMethod.call(this, ...args);
+        console.log(`LOG: Exiting method '${methodName}'.`)
+        return result;
+    }
+
+    return replacementMethod;
+}
+
+function bound(originalMethod: any, context: ClassMethodDecoratorContext) {
+    const methodName = context.name;
+    if (context.private) {
+        throw new Error(`'bound' cannot decorate private properties like ${methodName as string}.`);
+    }
+    context.addInitializer(function () {
+        this[methodName] = this[methodName].bind(this);
+    });
+}
+
+
+
+class Person {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    @bound
+    @loggedMethod
+    greet() {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
+}
+
+const p = new Person("Ron");
+p.greet();
+
+
+
 // {{## BEGIN simple ##}}
 function example(target: any) {
     console.log("Do something with 'target'", target);
@@ -43,6 +86,7 @@ function classDecorator<T extends {new(...args:any[]):{}}> (constructor:T) {
 class Greeter {
     property = "property";
     hello: string;
+
     constructor(m: string) {
         this.hello = m;
     }
